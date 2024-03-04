5a282e014132847693727c3739aec1715d28b855
emmanuelparungao <kvmanaig.wup@gmail.com>
2024-03-04 11:54:49 +0800

    my second commit

---
+++ register.js
@@ -0,0 +1,23 @@
+console.log("Hello World.");
+
+// string = "emj", "emj", "0125"
+// number = 24, 24.1, 1230e
+// boolean = "true", false, 1 or 0 - 1 = fale,>0
+//object = ["apple", "banana", "grape"]
+          // [1,2,3,4]
+          // [1' "apple", true]
+          // [
+          // [1,2,3]
+          // ["apple", "banana", "grapes"],
+          //]
+
+let txtUsername = document.querySelector("#txtUsername");
+let btnRegister = document.querySelector("#btnRegister");
+
+
+btnRegister.onclick = function(){ 
+    register(txtUsername.value,)
+}
+
+function register (username) {
+  }  console.log(username)
